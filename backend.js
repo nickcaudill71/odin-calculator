0f5df05f217a1db28f68d48872dc6df508528082
Nicholas Caudill <nick.c@coppermountaintech.com>
2025-06-04 16:19:12 -0400

    Add initial backend logic

---
+++ backend.js
@@ -0,0 +1,63 @@
+function addButtons(parent) {
+    const labels = 
+    [   '%', 'CE', 'C', 'DEL',
+        '7', '8', '9', 'x',
+        '4', '5', '6', '/',
+        '1', '2', '3', '-',
+        '.', '0', '=', '+'
+    ]
+
+    labels.forEach(element => {
+        let button = document.createElement('button');
+        button.textContent = element;
+        button.classList.add('button');
+        button.addEventListener('click', e => {
+            console.log(e.target.textContent)
+        })
+
+        parent.appendChild(button);
+    });
+}
+
+function operate(operation) {
+    a = parseFloat(operation.a);
+    b = parseFloat(operation.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        console.log("Invalid number in operation...");
+        return 'Error';
+    }
+
+    switch (operation.operant) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '/':
+            return divide(a, b);
+        case '*':
+            return multiply(a, b);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+let buttonDiv = document.querySelector('#buttons');
+addButtons(buttonDiv);
+
+
+let currentOperation = {a: '', b: '', operant: ''}
